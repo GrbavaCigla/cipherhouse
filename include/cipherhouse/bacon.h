b1e66fcab672bfb4b541eb3dc220cddc8f1b7cdc
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2020-08-05 21:30:22 +0200

    Fixed issue with previous commit

--- include/cipherhouse/bacon.h
+++ include/cipherhouse/bacon.h
@@ -5,5 +5,5 @@
 
 unsigned char *bacon_n_encrypt(const char *text, size_t textlen);
 unsigned char *bacon_n_decrypt(const char *text, size_t textlen);
-unsigned char *bacon_encrypt(const char *text) {;
-unsigned char *bacon_decrypt(const char *text) {;
+unsigned char *bacon_encrypt(const char *text);
+unsigned char *bacon_decrypt(const char *text);
--- tests/affine.c
+++ tests/affine.c
@@ -6,5 +6,9 @@ int main()
 {
     int encmsg = strcmp("insxchmrwbglqvafkpuzejotyd", affine_encrypt("abcdefghijklmnopqrstuvwxyz", 5, 8));
     int decmsg = strcmp("abcdefghijklmnopqrstuvwxyz", affine_decrypt("insxchmrwbglqvafkpuzejotyd", 5, 8));
-    return decmsg || encmsg;
+    int nencmsg = strcmp("insxchmrwbglqvafkpuzejotyd", affine_n_encrypt("abcdefghijklmnopqrstuvwxyz", 26, 5, 8));
+    int ndecmsg = strcmp("abcdefghijklmnopqrstuvwxyz", affine_n_decrypt("insxchmrwbglqvafkpuzejotyd", 26, 5, 8));
+
+
+    return decmsg || encmsg || ndecmsg || nencmsg;
 }
