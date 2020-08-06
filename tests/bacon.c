52edfcb5b290a7c95ca0731cce74e0a56723e2fc
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2020-08-06 13:34:43 +0200

    Removed new tests for bacon because of failure in test. Will rewrite it soon

--- tests/bacon.c
+++ tests/bacon.c
@@ -6,8 +6,6 @@ int main()
 {
     int encmsg = strcmp("aaaaaaaaabaaabaaaabbaabaaaababaabbaaabbbabaaaabaabababaababbabbaaabbababbbaabbbbbaaaabaaabbaababaabbbabaabababbabbababbbbbaaabbaab", bacon_encrypt("abcdefghijklmnopqrstuvwxyz"));
     int decmsg = strcmp("abcdefghijklmnopqrstuvwxyz", bacon_decrypt("aaaaaaaaabaaabaaaabbaabaaaababaabbaaabbbabaaaabaabababaababbabbaaabbababbbaabbbbbaaaabaaabbaababaabbbabaabababbabbababbbbbaaabbaab"));
-    int encmsg = strcmp("aaaaaaaaabaaabaaaabbaabaaaababaabbaaabbbabaaaabaabababaababbabbaaabbababbbaabbbbbaaaabaaabbaababaabbbabaabababbabbababbbbbaaabbaab", bacon_encrypt("abcdefghijklmnopqrstuvwxyz"));
-    int decmsg = strcmp("abcdefghijklmnopqrstuvwxyz", bacon_decrypt("aaaaaaaaabaaabaaaabbaabaaaababaabbaaabbbabaaaabaabababaababbabbaaabbababbbaabbbbbaaaabaaabbaababaabbbabaabababbabbababbbbbaaabbaab"));
 
     return decmsg || encmsg;
 }
